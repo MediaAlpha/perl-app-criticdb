ef180850e1c8653c6d9d50d49f135e7a771108ad
Brian Blackmore <brian@mediaalpha.com>
2026-06-01 07:52:06 -0700

    viv 0.0.5

--- lib/App/CriticDB/DB/Stor.pm
+++ lib/App/CriticDB/DB/Stor.pm
@@ -9,7 +9,7 @@ use Fcntl qw/:flock/;
 use File::Temp qw//;
 use Storable qw/nstore_fd fd_retrieve/;
 
-our $VERSION='0.0.4';
+our $VERSION='0.0.5';
 
 sub storfileUnchanged {
 	my ($self)=@_;
@@ -67,7 +67,7 @@ App::CriticDB::DB::Stor - Storable database for App::CriticDB
 
 =head1 VERSION
 
-Version 0.0.4
+Version 0.0.5
 
 =head1 SYNOPSIS
 
